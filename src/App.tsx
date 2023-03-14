3b581bca1bfff0ddefc48a711a9e345e1dae8483
NilsMillot <compte.mxs@hotmail.fr>
2023-03-14 17:36:23 +0100

    add boiler plate router

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import type { Component } from "solid-js";
 
 import logo from "./logo.svg";
 import styles from "./App.module.css";
+import { Route, Routes } from "@solidjs/router";
 
-const App: Component = () => {
+const Home: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -21,6 +22,30 @@ const App: Component = () => {
         </a>
       </header>
     </div>
+  );
+};
+
+const App: Component = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" component={Home} />
+        <Route
+          path="/about"
+          element={<div>This site was made with Solid</div>}
+        />
+      </Routes>
+    </>
   );
 };
 
