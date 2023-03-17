3b6150f10551615731cae630c68bbc4ecfdd15b9
NilsMillot <compte.mxs@hotmail.fr>
2023-03-17 18:06:38 +0100

    add 2 others cars img and add 404 page for site and for cars

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "@solidjs/router";
 import Footer from "./components/footer/Footer";
 
 import Navbar from "./components/navbar/Navbar";
-import Car from "./components/Car";
+import Car from "./components/car/Car";
 
 const App: Component = () => {
   const Home = lazy(() => import("./components/home/Home"));
@@ -18,7 +18,22 @@ const App: Component = () => {
           path="/about"
           element={<div>This site was made with Solid</div>}
         />
-        <Route path="*" element={<h1>404 Not found</h1>} />
+        <Route
+          path="*"
+          element={
+            <h1
+              style={{
+                display: "flex",
+                "justify-content": "center",
+                height: "75vh",
+                "align-items": "center",
+                "margin-top": "-100px",
+              }}
+            >
+              404 Not found
+            </h1>
+          }
+        />
       </Routes>
       <Footer></Footer>
     </>
