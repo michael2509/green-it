e7f3dafadd9fb86eb518e16f9d9b02902ea319a4
NilsMillot <compte.mxs@hotmail.fr>
2023-03-17 17:11:08 +0100

    WIP Product page

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { Component, lazy } from "solid-js";
 import { Route, Routes } from "@solidjs/router";
 
 import Navbar from "./components/Navbar";
+import Car from "./components/Car";
 
 const App: Component = () => {
   const Home = lazy(() => import("./components/Home"));
-
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" component={Home} />
+        <Route path="/car/:id" component={Car} />
         <Route
           path="/about"
           element={<div>This site was made with Solid</div>}
         />
+        <Route path="*" element={<h1>404 Not found</h1>} />
       </Routes>
     </>
   );
