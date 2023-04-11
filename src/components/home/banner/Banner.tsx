af59653bb0de97bff14565eb71a6ffa8360c8707
Michael De Madet <michaeldemadet@gmail.com>
2023-04-11 16:52:18 +0200

    improve responsive

--- src/components/home/banner/Banner.tsx
+++ src/components/home/banner/Banner.tsx
@@ -1,26 +1,30 @@
 import { Component, createSignal } from "solid-js";
 import styles from "./Banner.module.css";
 import banner from "../../../assets/images/home-banner-desktop.webp";
-import bannerMobile from "../../../assets/images/home-banner-mobile.png";
+import bannerMobile from "../../../assets/images/home-banner-mobile.avif";
 import Button from "../../button/Button";
 
 const Banner: Component = () => {
-    const [isSmallScreen, setIsSmallScreen] = createSignal(window.innerWidth <= 860);
+    const [isSmallScreen, setIsSmallScreen] = createSignal(window.innerWidth <= 768);
 
     window.addEventListener("resize", () => {
-        setIsSmallScreen(window.innerWidth <= 860);
+        setIsSmallScreen(window.innerWidth < 768);
     });
 
     return (
         <header class={styles.header}>
             <span class={styles.bannerTitle}>CONCESSIONNAIRE AUTOMOBILE</span>
-            <span class={styles.bannerSubTitle1}>Offres de voitures neuves ou d'occasions</span>
-            <span class={styles.bannerSubTitle2}>Rachat de vos véhicules</span>
+            {window.innerWidth > 480 && (
+                <>
+                    <span class={styles.bannerSubTitle1}>Offres de voitures neuves ou d'occasions</span>
+                    <span class={styles.bannerSubTitle2}>Rachat de vos véhicules</span>
+                </>
+            )}
             <span class={styles.bannerBtn}>
                 <Button linkTo="/car/1" small={isSmallScreen()}>Voir une offre</Button>
             </span>
             <picture>
-                <source class={styles.bannerImg} srcset={bannerMobile} media="(max-width: 860px)"></source>
+                <source class={styles.bannerImg} srcset={bannerMobile} media="(max-width: 768px)"></source>
                 <img class={styles.bannerImg} src={banner} alt="MDN" />
             </picture>
         </header>
